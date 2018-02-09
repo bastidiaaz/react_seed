364f907738ec91b29f6eb1bdbc620cbb2bc63059
bastidiaaz <basty.diaaz@gmail.com>
2018-02-09 21:14:36 +0100

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+ReactJS + Webpack + Babel6 + ESlint
+
+Instalación:
+
+Instalar `npm`:
+```
+sudo apt install npm
+```
+
+Instalar `node.js`:
+```
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+```
+
+Instalar `webpack`:
+```
+npm i webpack -S
+```
+
+Instalar `html-webpack-plugin`:
+```
+npm install html-webpack-plugin --save
+```
+
+Instalar `babel` y dependencias:
+```
+npm i babel-loader babel-preset-es2015 babel-preset-react -S
+```
+
+Instalar `react` y `react-dom`:
+```
+npm i react react-dom -S
+```
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlPlugin = require('html-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+
+module.exports = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlPlugin({ //Handle HTML files
+      template: 'src/client/app/index.html'
+    })
+  ]
+}
